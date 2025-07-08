08d6c90392bb4acf43dccc93005e50828f718e8d
CosyXD <123475234+CosyXD@users.noreply.github.com>
2025-07-08 21:20:47 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+function createResourcePack() {
+    var modelInput = document.getElementById('modelfile')
+    var textureInput = document.getElementById('texturefile')
+    var nameInput = document.getElementById('textinput').value
+    var texture = textureInput.files[0];
+    var model = modelInput.files[0];
+
+    var zip = new JSZip();
+
+    const packmetaText =
+`{
+  "pack": {
+    "pack_format": 46,
+    "description": "Template for resourcepacks. Put together by 100percentme"
+  }
+}`
+
+    var baseName =
+`{
+  "model": {
+    "type": "minecraft:model",
+    "model": "cavernermodels:item/${nameInput}"
+  }
+}`
+    var assetsF = zip.folder("assets") /* assetsF means assetsFolde */
+    var namespaceF = assetsF.folder("cavernermodels")
+    var itemsF = namespaceF.folder("items")
+    var modelsF = namespaceF.folder("models")
+    var texturesF = namespaceF.folder("textures")
+    var MitemF = modelsF.folder("item")
+    var TitemF = texturesF.folder("item")
+
+    zip.file('pack.mcmeta', packmetaText)
+
+    TitemF.file(texture.name, texture)
+    itemsF.file(nameInput + ".json", baseName)
+
+
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+      let json = JSON.parse(e.target.result);
+
+      json.textures["0"] = "cavernermodels:item/" + texture.name.replace(/\.[^/.]+$/, "");
+      json.textures["particle"] = "cavernermodels:item/" + texture.name.replace(/\.[^/.]+$/, "");
+
+      MitemF.file(model.name, JSON.stringify(json, null, 2));
+
+      zip.generateAsync({ type: "blob" }).then(function(content) {
+        saveAs(content, "example.zip");
+      });
+    };
+
+    reader.readAsText(model);
+}
